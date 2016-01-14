d592e9a8f61cf5ba59744ef6fceb3dd4ac3d8fe9
SweetNAMese <t.lyna.nguyen@gmail.com>
2016-01-14 13:28:20 -0500

    added student and instructor

---
+++ people.rb
@@ -0,0 +1,14 @@
+# Let's start by creating two classes: one called Student and another called Instructor.
+
+class Student < Person
+  def learn
+    puts "I get it!"
+
+end
+
+
+class Instructor < Person
+  def teach
+    puts "Everthing in Ruby is an Object"
+
+end
