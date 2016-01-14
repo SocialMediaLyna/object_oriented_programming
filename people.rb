05dc55c6e84099aa2798d4f2977748af06783fa2
SweetNAMese <t.lyna.nguyen@gmail.com>
2016-01-14 13:55:31 -0500

    instructor and student added

--- people.rb
+++ people.rb
@@ -1,14 +1,31 @@
 # Let's start by creating two classes: one called Student and another called Instructor.
 
+class Person
+  attr_accessor :name
+
+  def initialize(first_name, last_name)
+    @first_name = first_name
+    @last_name = last_name
+
+  end
+end
+
 class Student < Person
   def learn
     puts "I get it!"
-
+end
+  def speak
+    puts "Hi, my name is #{name}"
+  end
 end
 
 
 class Instructor < Person
   def teach
     puts "Everthing in Ruby is an Object"
+end
+  def speak
+    puts "Hi, my name is #{name}"
 
+  end
 end
